040630fc38adeb7cfc924f708aca2d6160d97f39
RMHIVSM <rmhivsm@ucl.ac.uk>
2021-04-14 13:32:56 +0100

    updating change

--- scripts/xml_script.py
+++ scripts/xml_script.py
@@ -2,9 +2,9 @@
 from pk_tables.xml_preprocessing import get_file_list, apply_to_all, create_jsonl
 #########
 
-file_list = get_file_list('../data/xml/selected_pmc/')
+#file_list = get_file_list('../data/xml/selected_pmc/')
 
-table_list= apply_to_all(file_list)
+#table_list= apply_to_all(file_list)
 
-create_jsonl(table_list, '../data/json/pmctables_json.jsonl')
+#create_jsonl(table_list, '../data/json/pmctables_json.jsonl')
 
